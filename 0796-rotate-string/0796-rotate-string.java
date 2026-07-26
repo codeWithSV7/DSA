d0b2af66fc6017a4b5c5f29b4b672a7ad484ba65
codeWithSV7 <238t1a4236@dietonline.in>
2026-07-26 22:13:17 +0530

    Time: 0 ms (100%), Space: 43.3 MB (53.65%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,10 +1,27 @@
+// class Solution {
+//     public boolean rotateString(String s, String goal) {
+//         if(s.length() != goal.length()){
+//             return false;
+//         }
+//         String doubled = s+s;
+//         return doubled.contains(goal);
+
+//     }
+// }
+
 class Solution {
     public boolean rotateString(String s, String goal) {
-        if(s.length() != goal.length()){
+        if (s.length() != goal.length())
             return false;
+        int n = s.length();
+        for (int k = 0; k < n; k++) {
+            int j = 0;
+            while (j < n && s.charAt((k + j) % n) == goal.charAt(j)) {
+                j++;
+            }
+            if (j == n)
+                return true;
         }
-        String doubled = s+s;
-        return doubled.contains(goal);
-
+        return false;
     }
 }
