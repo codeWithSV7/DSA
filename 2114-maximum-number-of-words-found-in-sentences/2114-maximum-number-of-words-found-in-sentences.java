f87a09201cbf40d910578fd1ad78ddd96e9a8f2e
codeWithSV7 <238t1a4236@dietonline.in>
2026-07-27 11:29:36 +0530

    Time: 3 ms (86.78%), Space: 44.8 MB (55.44%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.java
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.java
@@ -3,7 +3,7 @@ class Solution {
         int maxCount = 0;
         for(String sentence :sentences){
             int count = 1;
-            for(int ch : sentence.toCharArray()){
+            for(char ch : sentence.toCharArray()){
                 if(ch == ' '){
                     count++;
                 }
